b2cde2fd83216bcf9fff44930b1a7c4feacd7751
Ajay Sainy <sainyajay@gmail.com>
2017-11-27 02:21:56 -0400

    Error messages are displayed on login section

--- js/index.js
+++ js/index.js
@@ -1,53 +1,48 @@
+const URL = "http://localhost:1337";
+
 jQuery(document).ready(function($) {
-	$(".scroll").click(function(event){
-		event.preventDefault();
-		$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
-	});
-});	
+    $(".scroll").click(function(event) {
+        event.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
+    });
+});
 
 
 $(document).ready(function() {
-	$( "#user-login").click(function() {
-		var userid = $("#userid").val();
-		var password = $("#password").val();
+    $("#user-login").click(function() {
+        var userid = $("#userid").val();
+        var password = $("#password").val();
 
-		login(userid,password);
-	});
+        login(userid, password);
+    });
 
-	$().UItoTop({ easingType: 'easeOutQuart' });
+    $().UItoTop({ easingType: 'easeOutQuart' });
 
 });
 
 //Login function
-function login(userid,password) {
-	// var data = {
-	// 	userid:userid,
-	// 	password:password
-	// };
-
-	// $.post( "http://localhost:1337/api/login", data)
-	//  .done(function( response ) {
-	//  	console.log(response);	 	
-	//  	$.get("http://localhost:1337/api/create");
-	//   });
-
-	$.ajax({
-	   url: "http://localhost:1337/api/login",
-	   method:"POST",
-	   xhrFields: {
-	      withCredentials: true
-	   }
-	}).done(function( response ) {
-	 	console.log(response);	 	
-		$.ajax({
-		   url: "http://localhost:1337/api/create",
-		   method:"GET",
-		   xhrFields: {
-		      withCredentials: true
-		   }
-		}).done(function( response ) {
-		 	console.log(response);	 	
-		 	
-	  	});
-  	});
+function login(userid, password) {
+    $.ajax({
+        url: URL + "/api/login",
+        method: "POST",
+        data:{
+	   		"userid" : userid,
+	   		"password" : password
+	   	},
+        xhrFields: {
+            withCredentials: true
+        }
+    }).done(function(response, statusText, xhr) {
+        if (xhr.status == 200) {
+        	$("#login-error").empty();
+            //Open user home page
+            window.location.href = "user_home.html";
+        }
+    }).fail(function(response) {
+    	$("#login-error").empty();
+    	response.responseJSON.message.forEach(function(msg) {
+			$('#login-error').append("<span style='padding-left:1px;color:red;'>"+msg+"</spn><br/>");	
+		});
+        
+    });
 }
