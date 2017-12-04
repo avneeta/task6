a595fc64b00ac9527fe0f178a1a2aaab7673f1fa
Xin Yuan <xy1@andrew.cmu.edu>
2017-12-03 20:19:56 -0500

    Added customer profile page

--- js/index.js
+++ js/index.js
@@ -1,48 +1,53 @@
-const URL = "http://localhost:1337";
-
 jQuery(document).ready(function($) {
-    $(".scroll").click(function(event) {
-        event.preventDefault();
-        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 1000);
-    });
-});
+	$(".scroll").click(function(event){
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+	});
+});	
 
 
 $(document).ready(function() {
-    $("#user-login").click(function() {
-        var username = $("#username").val();
-        var password = $("#password").val();
+	$( "#user-login").click(function() {
+		var userid = $("#userid").val();
+		var password = $("#password").val();
 
-        login(username, password);
-    });
+		login(userid,password);
+	});
 
-    $().UItoTop({ easingType: 'easeOutQuart' });
+	$().UItoTop({ easingType: 'easeOutQuart' });
 
 });
 
 //Login function
-function login(username, password) {
-    $.ajax({
-        url: URL + "/api/login",
-        method: "POST",
-        data:{
-	   		"username" : username,
-	   		"password" : password
-	   	},
-        xhrFields: {
-            withCredentials: true
-        }
-    }).done(function(response, statusText, xhr) {
-        if (xhr.status == 200) {
-        	$("#login-error").empty();
-            //Open user home page
-            window.location.href = "user_home.html";
-        }
-    }).fail(function(response, statusText, xhr) {
-    	$("#login-error").empty();
-    	response.responseJSON.message.forEach(function(msg) {
-			$('#login-error').append("<span style='padding-left:1px;color:red;'>"+msg+"</spn><br/>");	
-		});
-        
-    });
+function login(userid,password) {
+	// var data = {
+	// 	userid:userid,
+	// 	password:password
+	// };
+
+	// $.post( "http://localhost:1337/api/login", data)
+	//  .done(function( response ) {
+	//  	console.log(response);	 	
+	//  	$.get("http://localhost:1337/api/create");
+	//   });
+
+	$.ajax({
+	   url: "http://localhost:1337/api/login",
+	   method:"POST",
+	   xhrFields: {
+	      withCredentials: true
+	   }
+	}).done(function( response ) {
+	 	console.log(response);	 	
+		$.ajax({
+		   url: "http://localhost:1337/api/create",
+		   method:"GET",
+		   xhrFields: {
+		      withCredentials: true
+		   }
+		}).done(function( response ) {
+		 	console.log(response);	 	
+		 	
+	  	});
+  	});
 }
